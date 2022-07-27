16d4ee682f5e159b53019f5cb8509efc3c36d30d
Adam Mill <hismajesty@theroyalwhee.com>
2022-07-27 08:35:25 -0400

    Update to latest Tricycle. Fix tests. Remove dev-depends that was added to depends. Add Usage to readme. Bump version.

--- test/mock/context.ts
+++ test/mock/context.ts
@@ -1,4 +1,4 @@
-import { Response, Request, JsonValue, HttpStatus, Context } from "@tricycle/tricycle";
+import Tricycle, { Response, Request, JsonValue, Context, RequestParams } from "@tricycle/tricycle";
 import { Platform } from "@tricycle/tricycle/dist/platform";
 import { Logger } from "winston";
 import { WinstonContext } from "../../src/middleware";
@@ -6,13 +6,17 @@ import { MockAzureContext, mockAzureLogger } from "./azurefunction";
 
 
 export class MockTricycleContext implements Context {
+    app: Tricycle;
+    response: Response;
+    request: Request;
+    platform: Platform;
     constructor() {
         this.platform = new Platform();
         this.platform.azureContext = new MockAzureContext();
     }
-    response: Response;
-    request: Request;
-    platform: Platform;
+    get params(): RequestParams {
+        throw new Error("Method not implemented.");
+    }
     get url(): string {
         throw new Error("Method not implemented.");
     }
@@ -22,16 +26,9 @@ export class MockTricycleContext implements Context {
     get body(): JsonValue | undefined {
         throw new Error("Method not implemented.");
     }
-    set body(value: JsonValue | undefined) {
+    get status(): number | undefined {
         throw new Error("Method not implemented.");
     }
-    get status(): HttpStatus | undefined {
-        throw new Error("Method not implemented.");
-    }
-    set status(value: HttpStatus | undefined) {
-        throw new Error("Method not implemented.");
-    }
-
 }
 
 export class MockWinstonContext extends MockTricycleContext implements WinstonContext {
